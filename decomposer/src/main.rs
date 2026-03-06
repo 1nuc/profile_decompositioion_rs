aba78c5cc3d5e284a3826ef3abb414404bb7ff76
1nuc <ahmedalyafai85@gmail.com>
2026-03-07 03:47:12 +0800

    load file

--- decomposer/src/main.rs
+++ decomposer/src/main.rs
@@ -1,3 +1,13 @@
+use std::{default, env, fs, path::{Path, PathBuf}};
+use glob::glob;
+use polars::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    // let mut paths: Vec<_>=glob("../../../datasets").unwrap().filter_map(Result::ok).collect();
+    let args= ScanArgsParquet{
+        low_memory: true,
+        cache: false,
+        ..Default::default()
+    };
+    let data=LazyFrame::scan_parquet("../../../datasets/*.parquet", args).expect("error");
+    let length=data.select([len()]).with_streaming(true).collect();
 }
