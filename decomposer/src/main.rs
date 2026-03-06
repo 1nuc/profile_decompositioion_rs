970ff786924c4018409d8d70bee6b25d309f1625
1nuc <ahmedalyafai85@gmail.com>
2026-03-07 07:01:59 +0800

    additive data

--- decomposer/src/main.rs
+++ decomposer/src/main.rs
@@ -8,6 +8,25 @@ fn main() {
         cache: false,
         ..Default::default()
     };
-    let data=LazyFrame::scan_parquet("../../../datasets/*.parquet", args).expect("error");
-    let length=data.select([len()]).with_streaming(true).collect();
+    let data=LazyFrame::scan_parquet("../../../src/input/*.parquet", args.clone()).expect("error");
+    let buildings=data.clone().select([col("bldg_id")]).with_streaming(true).collect().unwrap().column("bldg_id").unwrap().to_owned();
+
+    let meta_data=LazyFrame::scan_parquet("../../../metadata/MetaData.parquet", args.clone()
+        ).expect("error reading the file").with_columns(
+        [col("bldg_id").cast(DataType::Int32)]).filter(
+        col("bldg_id").is_in(lit(buildings))).select([col("in.occupants").cast(DataType::Int32),
+        col("in.state"), col("in.county"), 
+        col("in.representative_income"),
+        col("in.area_median_income"),
+        col("in.income"),
+        col("in.income_recs_2020"),
+        col("in.income_recs_2015"),
+        col("in.building_america_climate_zone"),
+        col("in.ashrae_iecc_climate_zone_2004_sub_cz_split").alias("cliamte_zone"),
+        col("in.bedrooms").cast(DataType::Int32),
+        col("in.tenure"),
+        col("bldg_id").cast(DataType::UInt32),
+        col("in.household_has_tribal_persons")]);
+    let data=data.join(meta_data, [col("bldg_id")], [col("bldg_id")], Default::default()).collect().unwrap();
+    println!("{:?}", data);
 }
