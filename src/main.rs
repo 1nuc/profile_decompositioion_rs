730dfa90c909239f1946db58e537f55e538d8905
1nuc <ahmedalyafai85@gmail.com>
2026-03-07 08:26:15 +0800

    add tap crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use polars::prelude::*;
+use std::time;
+use tap::Pipe;
+use polars::{prelude::*};
 fn main() {
     // let mut paths: Vec<_>=glob("../../../datasets").unwrap().filter_map(Result::ok).collect();
     let args= ScanArgsParquet{
@@ -6,13 +8,11 @@ fn main() {
         cache: false,
         ..Default::default()
     };
-    let data=LazyFrame::scan_parquet("../../../src/input/*.parquet", args.clone()).expect("error");
-    let buildings=data.clone().select([col("bldg_id")]).with_streaming(true).collect().unwrap().column("bldg_id").unwrap().to_owned();
-
-    let meta_data=LazyFrame::scan_parquet("../../../metadata/MetaData.parquet", args.clone()
+    let t=time::Instant::now();
+    let meta_data=LazyFrame::scan_parquet("../../metadata/MetaData.parquet", Default::default()
         ).expect("error reading the file").with_columns(
-        [col("bldg_id").cast(DataType::Int32)]).filter(
-        col("bldg_id").is_in(lit(buildings))).select([col("in.occupants").cast(DataType::Int32),
+        [col("bldg_id").cast(DataType::Int32)]).
+        select([col("in.occupants").cast(DataType::Int32),
         col("in.state"), col("in.county"), 
         col("in.representative_income"),
         col("in.area_median_income"),
@@ -25,5 +25,11 @@ fn main() {
         col("in.tenure"),
         col("bldg_id").cast(DataType::UInt32),
         col("in.household_has_tribal_persons")]);
-    let data=data.join(meta_data, [col("bldg_id")], [col("bldg_id")], Default::default()).collect().unwrap();
+
+    let data=LazyFrame::scan_parquet("../../src/input/*.parquet",Default::default() 
+        ).expect("error").join(
+        meta_data, [col("bldg_id")],
+        [col("bldg_id")], Default::default()
+        ).drop_nulls(None);
+    println!("first time: {:?}", t.elapsed());
 }
