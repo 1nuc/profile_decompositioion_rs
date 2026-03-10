86f7717926927c81669e39e5551389266ba94f50
1nuc <ahmedalyafai85@gmail.com>
2026-03-11 00:47:49 +0800

    save updates

--- decomposer_engine/src/preprocessor_engine.rs
+++ decomposer_engine/src/preprocessor_engine.rs
@@ -1,4 +1,4 @@
-use polars::prelude::LazyFrame;
+use polars::prelude::*;
 
 struct Preprocessor <'a>{
     x: LazyFrame,
@@ -7,11 +7,27 @@ struct Preprocessor <'a>{
     test_size: f32,
     rnd_state: u64,
     labels: Vec<&'a str>,
+    x_labels_size: u32,
+    y_labels_size: u32,
+    x_labels: Vec<&'a str>,
+    y_labels: Vec<&'a str>,
 }
 impl <'a> Preprocessor<'a>{
-    fn new();
-    fn extract_x_nd_y();
-    fn split_x_y();
-    fn splitting();
-    fn labelling();
+    fn new(&self);
+
+    fn extract_x_nd_y(d: LazyFrame) -> (LazyFrame, LazyFrame){
+        let x=d.clone().select([
+            col(
+                PlSmallStr::from("^day*|^hour*|^week*|^month*|^time*|^quarter|^IsWeekend|^in.*|^Short|^climatezone$")
+                ),col("out.electricity.total.energy_consumption").alias("total_usage")
+        ]);
+        let y=d.clone().select([
+            col(
+                PlSmallStr::from("^out.electricity.*..kwh$")
+            )]);
+        (x,y)
+    }
+    fn split_x_y(&self);
+    fn splitting(&self);
+    fn labelling(&self);
 }
