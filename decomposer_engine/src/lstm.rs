2fb1958a157270e0ce7f0dac14c96ab3562395d2
1nuc <ahmedalyafai85@gmail.com>
2026-03-17 02:43:51 +0800

    save notes

--- decomposer_engine/src/lstm.rs
+++ decomposer_engine/src/lstm.rs
@@ -1,11 +1,20 @@
-use burn::{nn::Lstm, prelude::Backend, *};
+use burn::{nn::{Linear, Lstm}, prelude::Backend, *};
 
-struct NucLstmackend<B> where  B: Backend{
-    model: Lstm<B>,
+struct NucLstmConfig{
+    input_size: usize,
+    output_size: usize,
+    hidden_size: usize,
+    num_layers: usize,
+    dropout: f32,
 }
 
-impl <B: Backend> NucLstmackend<B>{
-    fn new(){
+struct NucLstm<B> where  B: Backend{
+    model: Lstm<B>,
+    output_model: Linear<B>,
+}
+
+impl <B: Backend>NucLstm<B>{
+    fn forward(){
 
     }
 }
