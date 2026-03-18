3c114ca7bba7f092871853193baa4b0d6549ece0
1nuc <ahmedalyafai85@gmail.com>
2026-03-19 04:05:10 +0800

    save notes

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ fn main() {
     let data_source=Nrel::init();
     let mut data=data_source.data;
     let encoded_data=data.encode_categoricals();
-    let preprocessor=Preprocessor::new(encoded_data.clone(), 42, 0.3);
-    let (mut x_train, mut x_test, mut y_train, y_test)=preprocessor.split_x_y();
+    // let a= encoded_data.();
+
+    // let preprocessor=Preprocessor::new(encoded_data.clone(), 42, 0.3);
+    
+    // let (mut x_train, mut x_test, mut y_train, y_test)=preprocessor.split_x_y();
     // let d_train=x_train.to_matrix(true);
     // let d_test=x_test.to_matrix(true);
     // let mut xgb=Xgb::new(d_train, d_test);
