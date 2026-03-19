4e1697f368763d557bc76de269f3ced57a98d9ab
1nuc <ahmedalyafai85@gmail.com>
2026-03-19 11:51:44 +0800

    save notes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use decomposer_engine::{Actions, data_engine::*, preprocessor_engine::Preprocessor, xgb::Xgb, lstm::*}; 
+use decomposer_engine::{Actions, EagerActions, data_engine::*, lstm::*, preprocessor_engine::Preprocessor, xgb::Xgb}; 
 use polars::prelude::*;
 use tap::Conv;
 
 fn main() {
     let data_source=Nrel::init();
     let mut data=data_source.data;
-    let encoded_data=data.return_time_sequenced().encode_categoricals();
-    println!("{:?}", encoded_data.collect().unwrap());
+    let encoded_data=data.encode_categoricals().return_time_sequenced();
+    println!("{:?}", encoded_data.collect().unwrap().select_sequence());
     // let preprocessor=Preprocessor::new(encoded_data.clone(), 42, 0.3);
     // let (mut x_train, mut x_test, mut y_train, y_test)=preprocessor.split_x_y();
     // let d_train=x_train.to_matrix(true);
