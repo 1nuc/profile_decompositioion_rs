72e0ee96aa4b45ed0cfd7f9138df655e77aa3397
1nuc <ahmedalyafai85@gmail.com>
2026-03-20 10:34:03 +0800

    save notes

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,22 @@ use decomposer_engine::{Actions, EagerActions, data_engine::*, lstm::*, preproce
 use ndarray::{Array3, s};
 use polars::prelude::*;
 use tap::Conv;
+use burn::{Tensor, backend::{self, wgpu::WgpuDevice}, prelude::Backend, tensor::TensorData};
 
-fn main() {
+fn main(){
     let data_source=Nrel::init();
     let mut data=data_source.data;
     let encoded_data=data.clone().encode_categoricals().return_time_sequenced();
     let d=encoded_data.collect().unwrap();
-    let cols=d.return_y_columns();
-    let sequenced_data=d.select_sequence(cols.clone());
-    let samples=sequenced_data.height();
-    let data=sequenced_data.explode(
-        cols.clone(), ExplodeOptions {
-            empty_as_null: false,
-            keep_nulls: false 
-        }).unwrap().to_ndarray::<Float32Type>(Default::default()).unwrap();
-    let array: Array3<f32>=data.to_shape((samples,96, cols.len())).unwrap().to_owned();
-    let arr=array.slice(s![1,..,..]).to_vec();
-    println!("{:?}", array.slice(s![0,..,..]));
+    let binding=d.clone();
+    let cols=binding.return_y_columns();
+    let samples=d.height();
+    let sequenced_data=d.select_sequence(cols.clone(), samples);
+    let arr=sequenced_data.slice(s![3,..,..]).to_owned().into_raw_vec_and_offset().0.chunks(cols.len()).map(|x| x.to_vec()).collect::<Vec<Vec<f32>>>();
+    let device=WgpuDevice::default();
+    type MyBackend=backend::Wgpu<f32,i32>;
+    let tensor=Tensor::<MyBackend, 2>::from_data(TensorData::new(arr,[1,96,cols.len()]), &device);
+    println!("{:?}",tensor);
     // let preprocessor=Preprocessor::new(encoded_data.clone(), 42, 0.3);
     // let (mut x_train, mut x_test, mut y_train, y_test)=preprocessor.split_x_y();
     // let d_train=x_train.to_matrix(true);
