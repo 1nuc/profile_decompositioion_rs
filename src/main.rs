75a2d817ca6efa3ad208e521cb6c2d0ceb1b769d
1nuc <ahmedalyafai85@gmail.com>
2026-03-22 18:45:09 +0800

    save notes

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,12 @@ fn main(){
     let encoded_data=data.clone().encode_categoricals().return_time_sequenced().collect().unwrap();
     let data_fraction=encoded_data.clone().sample_frac(
         &Series::new("fraction".into(),[1]), false, false, Some(42)).unwrap();
-    let batches=encoded_data.height();
-    let cols=encoded_data.return_x_columns();
-    println!("{:?}", cols);
-    let data=encoded_data.select_sequence(cols, batches);
-    println!("{:?}", data);
-    // let validation_size=encoded_data.clone().height() as f32 * 0.3;
-    // let train_size=encoded_data.clone().height() as f32 * 0.7;
-    // let train_data=encoded_data.clone().head(Some(train_size as usize));
-    // let test_data=encoded_data.clone().tail(Some(validation_size as usize));
-    // let model_config=lstm::NrelConfig::new(AdamWConfig::new());
-    // model_config.train(train_data, test_data, "artifact_dir");
+    let validation_size=encoded_data.clone().height() as f32 * 0.3;
+    let train_size=encoded_data.clone().height() as f32 * 0.7;
+    let train_data=encoded_data.clone().head(Some(train_size as usize));
+    let test_data=encoded_data.clone().tail(Some(validation_size as usize));
+    let model_config=lstm::NrelConfig::new(AdamWConfig::new());
+    model_config.train(train_data, test_data, "artifact_dir");
     // let train_data=data_fraction.tail(length)
     // let preprocessor=Preprocessor::new(encoded_data.clone(), 42, 0.3);
     // let (mut x_train, mut x_test, mut y_train, y_test)=preprocessor.split_x_y();
