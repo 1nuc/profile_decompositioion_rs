b7ee72a7925301349e46d8f629d951b96f3a1355
1nuc <ahmedalyafai85@gmail.com>
2026-03-24 08:14:57 +0800

    save notes

---
+++ decomposer_engine/src/dl/controller.rs
@@ -0,0 +1,8 @@
+//TODO: Define the logic for training
+//TODO: Define the logic for testing and validation
+//TODO: Must make a function to split the dat into train, test and split
+//TODO: steps:
+//1. One struct that contains the data that will be red through the data engine
+//2. define the default implementation for the controller, the data backend, and many more
+//3. method for training and predicting
+//4. catch the method for the metrics
