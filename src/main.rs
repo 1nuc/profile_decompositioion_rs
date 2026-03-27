9396116dcbdaff81ab42550e2f8efc99279ad092
1nuc <ahmedalyafai85@gmail.com>
2026-03-28 04:29:05 +0800

    make the file readings function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::{ffi::OsString, fs::{read_dir}};
+
 use decomposer_engine::{Actions, EagerActions, data_engine::*, dl::controller::Controller, xgb};
 
 fn main(){
-    let data_source=Nrel::init();
-    let data=data_source.data;
-    let mut encoded_data=data.clone().encode_categoricals();
+    // let data_source=Nrel::init();
+    // let data=data_source.data;
+    // let mut encoded_data=data.clone().encode_categoricals();
+    let dir=read_dir("../../datasets").unwrap();
+    
+    let file_names=dir.map(|x| x.unwrap().file_name()
+        ).map(|x| x.to_str().unwrap().to_owned()).collect::<Vec<String>>();
+    println!("{:?}", file_names.len());
     // --- Xgboost Model
-    xgb::Xgb::runner(encoded_data);
+    // xgb::Xgb::runner(encoded_data);
 
     // ---- Deep learning Models
     // let s=encoded_data.clone().collect().unwrap();
