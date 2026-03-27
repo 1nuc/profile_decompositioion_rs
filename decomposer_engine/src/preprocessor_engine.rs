5e8ffb37d58e019f7688b81e273ae96986869964
1nuc <ahmedalyafai85@gmail.com>
2026-03-27 08:57:05 +0800

    save notes

--- decomposer_engine/src/preprocessor_engine.rs
+++ decomposer_engine/src/preprocessor_engine.rs
@@ -1,7 +1,8 @@
+use ndarray::Data;
 use polars::prelude::*;
 use rand::{SeedableRng, rngs::SmallRng, seq::SliceRandom};
 
-pub struct Preprocessor {
+pub struct Preprocessor<'a>{
     pub x: DataFrame,
     pub y: DataFrame,
     pub n: usize,
@@ -9,36 +10,34 @@ pub struct Preprocessor {
     pub y_n: usize,
     pub test_size: f32,
     pub rnd_state: u64,
-    pub labels: Vec<String>,
-    pub x_labels: Vec<String>,
-    pub y_labels: Vec<String>,
+    pub labels: Vec<&'a str>,
+    pub x_labels: Vec<&'a str>,
+    pub y_labels: Vec<&'a str>,
     pub x_labels_size: usize,
     pub y_labels_size: usize,
 }
 
-impl Preprocessor {
-    pub fn new(d: DataFrame, rnd_state_: u64, test_size_: f32) -> Self {
+impl <'a> Preprocessor<'a>{
+    pub fn new(d: LazyFrame, rnd_state_: u64, test_size_: f32) -> Self {
         let (x_, y_) = Self::extract_x_nd_y(d.clone());
         let d_ = d.clone().collect().unwrap();
-        let x_d = x_.clone().collect().unwrap();
-        let y_d = y_.clone().collect().unwrap();
         Self {
             x: x_.clone(),
             y: y_.clone(),
             n: d_.height(),
-            x_n: x_d.height(),
-            y_n: y_d.height(),
+            x_n: x_.height(),
+            y_n: y_.height(),
             test_size: test_size_,
             rnd_state: rnd_state_,
-            labels: Self::extract_labels(d),
+            labels: Self::extract_labels(d_),
             x_labels: Self::extract_labels(x_.clone()),
             y_labels: Self::extract_labels(y_.clone()),
-            x_labels_size: x_d.shape().1,
-            y_labels_size: y_d.shape().1,
+            x_labels_size: x_.shape().1,
+            y_labels_size: y_.shape().1,
         }
     }
 
-    fn extract_x_nd_y(d: DataFrame) -> (LazyFrame, LazyFrame) {
+    fn extract_x_nd_y(d: LazyFrame) -> (DataFrame, DataFrame) {
         let x=d.clone().select([
             col(
                 PlSmallStr::from("^day*|^hour*|^week*|^month*|^time*|^quarter|^IsWeekend|^in.*|^Short|^climate_zone$")
@@ -47,7 +46,7 @@ impl Preprocessor {
         Selector::ByName {
             names: Arc::new([PlSmallStr::from_str("in.sqft")]),
             strict: true
-        });
+        }).collect().unwrap();
         let y = d
             .clone()
             .select([col(PlSmallStr::from("^out.electricity.*..kwh$"))])
@@ -63,20 +62,15 @@ impl Preprocessor {
                     PlSmallStr::from_str("out.electricity.ev_charging.energy_consumption..kwh"),
                 ]),
                 strict: true,
-            });
+            }).collect().unwrap();
         (x, y)
     }
 
-    fn extract_labels(d: LazyFrame) -> Vec<String> {
-        d.clone()
-            .collect_schema()
-            .unwrap()
-            .iter_names()
-            .map(|x| x.as_str().to_string())
-            .collect::<Vec<String>>()
+    fn extract_labels(d: DataFrame) -> Vec<&'a str> {
+        d.get_column_names().iter().map(|x| x.to_owned().as_str()).collect::<Vec<&str>>()
     }
 
-    pub fn split_x_y(&self) -> (LazyFrame, LazyFrame, LazyFrame, LazyFrame) {
+    pub fn split_x_y(&self) -> (DataFrame, DataFrame, DataFrame, DataFrame) {
         let x_test_n = self.x_n as f32 * self.test_size;
         let x_train_n = self.x_n as f32 - x_test_n;
 
@@ -89,7 +83,7 @@ impl Preprocessor {
         (x_train, x_test, y_train, y_test)
     }
 
-    fn splitting(&self, d: LazyFrame, n: usize, x_n: f32) -> (LazyFrame, LazyFrame) {
+    fn splitting(&self, d: DataFrame, n: usize, x_n: f32) -> (DataFrame, DataFrame) {
         let mut arr: Vec<u32> = (0..n as u32).collect();
         let seed_rng = &mut <SmallRng as SeedableRng>::seed_from_u64(self.rnd_state);
         arr.shuffle(seed_rng);
@@ -98,18 +92,12 @@ impl Preprocessor {
         let t = ChunkedArray::from_slice("new".into(), test_arr);
         let r = ChunkedArray::from_slice("new".into(), train_arr);
         let test_t = d
-            .clone()
-            .collect()
-            .unwrap()
             .take(&t)
             .expect("error fetching the testing data");
         let train_t = d
-            .clone()
-            .collect()
-            .unwrap()
             .take(&r)
             .expect("error fetching the training data");
-        (train_t.lazy(), test_t.lazy())
+        (train_t, test_t)
     }
 
     // fn labelling(&self);
