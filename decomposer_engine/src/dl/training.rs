ebfc4c9438867356209075e99f358f7a71bcaa92
1nuc <ahmedalyafai85@gmail.com>
2026-03-28 09:27:26 +0800

    save notes

--- decomposer_engine/src/dl/training.rs
+++ decomposer_engine/src/dl/training.rs
@@ -1,7 +1,6 @@
-use burn::{config::Config, data::dataloader::DataLoaderBuilder, module::{Module}, optim::AdamWConfig, record::{CompactRecorder}, tensor::backend::AutodiffBackend, train::{Learner, SupervisedTraining,metric::{LossMetric}}};
-use ndarray::Data;
+use burn::{config::Config, data::dataloader::{DataLoader, DataLoaderBuilder, batcher::Batcher}, module::Module, optim::AdamWConfig, record::CompactRecorder, tensor::backend::AutodiffBackend, train::{Learner, SupervisedTraining,metric::LossMetric}, record::Recorder};
 use polars::{frame::DataFrame};
-use std::{fmt::Debug, fs::*, path::Path};
+use std::{fmt::Debug, fs::*, path::Path, sync::Arc};
 
 use crate::dl::{dataset::{NrelBatcher, NrelDataset}, models::{bi_lstm::NucBiLstmConfig, hybrid_models::{Seq2SeqConfig, Seq2SeqRecord}, lstm::NucLstmConfig, stacked_bi_lstm::StackedBiLstmConfig, stacked_lstm::StackedLstmConfig}};
 
@@ -35,28 +34,13 @@ impl NrelConfig{
             create_dir_all(artifact_dir);
         }
     }
+
     pub fn train<B: AutodiffBackend>(&self, train_data: DataFrame, test_data: DataFrame, artifact_dir: &str, device: B::Device)
     {
         self.create_artifact_dir::<B>(artifact_dir, train_data.clone(), test_data.clone(), device.clone());
-        //TODO: split the data into train and validate
-        let train_data=NrelDataset::new(train_data);
-        let test_data=NrelDataset::new(test_data);
-        let batcher=NrelBatcher::<B>::new(device.clone());
-        let test_batcher=NrelBatcher::<B::InnerBackend>::new(device.clone());
-        // Train Data
-        let train_loader=DataLoaderBuilder::new(batcher)
-            .batch_size(self.batch_size)
-            .num_workers(self.workers)
-            .shuffle(self.seed)
-            .build(train_data);
-        //Test Data
-        let test_loader=DataLoaderBuilder::new(test_batcher)
-            .batch_size(self.batch_size)
-            .num_workers(self.workers)
-            .shuffle(self.seed)
-            .build(test_data);
-        //TODO: build the model
         let model=self.model.init::<B>(device);
+        let (train_loader, test_loader)=self.prepare_training(train_data, test_data, &device);
+        // Initiate the training
         let train=SupervisedTraining::new(artifact_dir, train_loader, test_loader)
             .metric_train_numeric(LossMetric::new())
             .metric_valid_numeric(LossMetric::new())
@@ -77,27 +61,12 @@ impl NrelConfig{
 
         // using compact recorder, load the last saved state of the model
         let record: Seq2SeqRecord<B>= CompactRecorder::new().load(
-            format!("{artifact_dir}/model").into(), &device).expect("training model should exist first");
+            format!("{artifact_dir}/model").into(), &device).expect("First batch is not trained yet");
 
         // load and initialize the model for test 
         let model=config.model.init::<B>(device.clone()).load_record(record);
+        let (train_loader, test_loader)=self.prepare_training(train_data, test_data, &device);
 
-        let train_data=NrelDataset::new(train_data);
-        let test_data=NrelDataset::new(test_data);
-        let batcher=NrelBatcher::<B>::new(device.clone());
-        let test_batcher=NrelBatcher::<B::InnerBackend>::new(device.clone());
-        // Train Data
-        let train_loader=DataLoaderBuilder::new(batcher)
-            .batch_size(self.batch_size)
-            .num_workers(self.workers)
-            .shuffle(self.seed)
-            .build(train_data);
-        //Test Data
-        let test_loader=DataLoaderBuilder::new(test_batcher)
-            .batch_size(self.batch_size)
-            .num_workers(self.workers)
-            .shuffle(self.seed)
-            .build(test_data);
         //TODO: build the model
         let train=SupervisedTraining::new(artifact_dir, train_loader, test_loader)
             .metric_train_numeric(LossMetric::new())
@@ -111,6 +80,33 @@ impl NrelConfig{
         self.save(format!("{artifact_dir}/config.json").as_str()).unwrap();
         //TODO: Save the model results
         result.model.save_file(format!("{artifact_dir}/model"), &CompactRecorder::new()).expect("Error in saving the trained model");
+    }
 
+    fn prepare_training<B: AutodiffBackend> 
+        (&self,
+         train_data: DataFrame,
+         test_data: DataFrame,
+         device: &B::Device) ->
+            (Arc<dyn DataLoader<NrelBatcher<B>::Batch>>,
+             Arc<dyn DataLoader<<NrelBatcher<B::InnerBackend> as Batcher>::Batch>>){
+
+            //TODO: split the data into train and validate
+            let train_data=NrelDataset::new(train_data);
+            let test_data=NrelDataset::new(test_data);
+            let batcher=NrelBatcher::<B>::new(device.clone());
+            let test_batcher=NrelBatcher::<B::InnerBackend>::new(device.clone());
+            // Train Data
+            let train_loader=DataLoaderBuilder::new(batcher)
+                .batch_size(self.batch_size)
+                .num_workers(self.workers)
+                .shuffle(self.seed)
+                .build(train_data);
+            //Test Data
+            let test_loader=DataLoaderBuilder::new(test_batcher)
+                .batch_size(self.batch_size)
+                .num_workers(self.workers)
+                .shuffle(self.seed)
+                .build(test_data);
+             (train_loader, test_loader)
     }
 }
