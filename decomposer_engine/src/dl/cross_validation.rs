f28515ecdf69cf5a9c27dc188a0e84dae0b14120
1nuc <ahmedalyafai85@gmail.com>
2026-04-13 18:28:19 +0800

    save notes

--- decomposer_engine/src/dl/cross_validation.rs
+++ decomposer_engine/src/dl/cross_validation.rs
@@ -70,4 +70,83 @@ impl CrossValidate{
         });
         (train_sets, test_sets)
     }
+    
+    #[allow(unused_must_use)]
+    fn create_artifact_dir<B: AutodiffBackend>(
+        &self,
+        artifact_dir: &str,
+        train_data: DataFrame,
+        test_data: DataFrame,
+        device: B::Device,
+    ) {
+        let artifact_path = Path::new(artifact_dir);
+        if artifact_path.exists() {
+            self.inference_learning::<B>(artifact_dir, train_data, test_data, device);
+        } else {
+            create_dir_all(artifact_dir);
+        }
+    }
+
+    pub fn train<B: AutodiffBackend>(
+        &self,
+        train_data: DataFrame,
+        test_data: DataFrame,
+        artifact_dir: &str,
+        device: B::Device,
+    ) {
+        self.create_artifact_dir::<B>(
+            artifact_dir,
+            train_data.clone(),
+            test_data.clone(),
+            device.clone(),
+        );
+        let model = self.model.init::<B>(device.clone());
+        let (train_loader, test_loader) = self.prepare_training::<B>(train_data, test_data, &device);
+        // Initiate the training
+        let train = SupervisedTraining::new(artifact_dir, train_loader, test_loader)
+            .metric_train_numeric(LossMetric::new())
+            .metric_valid_numeric(LossMetric::new())
+            .with_file_checkpointer(CompactRecorder::new())
+            .num_epochs(self.num_epoch)
+            .summary();
+        let result = train.launch(Learner::new(model, self.opt.init(), 1e-3));
+        //TODO: save the configurations
+        self.save(format!("{artifact_dir}/config.json").as_str())
+            .unwrap();
+        //TODO: Save the model results
+        result
+            .model
+            .save_file(format!("{artifact_dir}/model"), &CompactRecorder::new())
+            .expect("Error in saving the trained model");
+    }
+
+    fn prepare_training<B: AutodiffBackend>(
+        &self,
+        train_data: DataFrame,
+        test_data: DataFrame,
+        device: &B::Device,
+    ) -> (
+        Arc<dyn DataLoader<B, NrelBatch<B>>>,
+        Arc<dyn DataLoader<B::InnerBackend, NrelBatch<B::InnerBackend>>>,
+    ) {
+        //TODO: split the data into train and validate
+        let train_data = NrelDataset::new(train_data);
+        let test_data = NrelDataset::new(test_data);
+        let batcher = NrelBatcher::<B>::new(device.clone());
+        let test_batcher = NrelBatcher::<B::InnerBackend>::new(device.clone());
+        // Train Data
+        let train_loader = DataLoaderBuilder::new(batcher)
+            .batch_size(self.batch_size)
+            .num_workers(self.workers)
+            .shuffle(self.seed)
+            .build(train_data);
+        //Test Data
+        let test_loader = DataLoaderBuilder::new(test_batcher)
+            .batch_size(self.batch_size)
+            .num_workers(self.workers)
+            .shuffle(self.seed)
+            .build(test_data);
+        (train_loader, test_loader)
+    }
+    fn train_folds
 }
