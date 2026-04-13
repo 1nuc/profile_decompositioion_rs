8ef48f51e7fc763b2cc6b79a8d741fa9dd724ac8
1nuc <ahmedalyafai85@gmail.com>
2026-04-13 22:11:42 +0800

    save notes

--- decomposer_engine/src/dl/cross_validation.rs
+++ decomposer_engine/src/dl/cross_validation.rs
@@ -1,10 +1,10 @@
 use std::{fs::{File, copy, create_dir, create_dir_all}, path::{Path, PathBuf}, sync::Arc};
-
-use burn::{config::Config, data::dataloader::DataLoader, nn::{BiLstm, Lstm}, optim::AdamConfig, prelude::Backend, record::CompactRecorder, tensor::backend::AutodiffBackend, train::{Learner, SupervisedTraining, metric::LossMetric}};
-use polars::{frame::DataFrame, prelude::{IntoLazy, LazyFrame, col, lit}, *};
+use burn::prelude::Module;
+use burn::{config::Config, data::dataloader::{DataLoader, DataLoaderBuilder}, optim::AdamWConfig, prelude::Backend, record::CompactRecorder, tensor::backend::AutodiffBackend, train::{Learner, SupervisedTraining, metric::LossMetric}};
+use polars::{frame::DataFrame, prelude::{IntoLazy, LazyFrame, col, lit}};
 use rand::seq::SliceRandom;
 
-use crate::dl::{controller::Controller, dataset::{NrelBatch, NrelBatcher, NrelDataset}, models::{bi_lstm::{NucBiLstm, NucBiLstmConfig}, hybrid_models::{Seq2Seq, Seq2SeqConfig}, lstm::{NucLstm, NucLstmConfig}, stacked_bi_lstm::{StackedBiLstmConfig, StackedBilstm}, stacked_lstm::{StackedLstmConfig, Stackedlstm}}};
+use crate::dl::{controller::Controller, dataset::{NrelBatch, NrelBatcher, NrelDataset}, models::{bi_lstm::{NucBiLstmConfig}, hybrid_models::{Seq2SeqConfig}, lstm::{NucLstmConfig}, stacked_bi_lstm::{StackedBiLstmConfig}, stacked_lstm::{StackedLstmConfig}}};
 
 pub struct CrossValidate{
     pub training_sets: Vec<DataFrame>,
@@ -75,21 +75,33 @@ impl CrossValidate{
 }
 #[derive(Debug, Config)]
 pub struct CrossModels{
-    pub lsmt: NucLstmConfig,
-    pub bi_lst: NucBiLstmConfig,
+    pub lstm: NucLstmConfig,
+    pub bi_lstm: NucBiLstmConfig,
     pub stacked_lstm: StackedLstmConfig,
     pub stacked_bi_lstm:StackedBiLstmConfig,
     pub seq2seq: Seq2SeqConfig,
     pub num_epoch: usize,
-    #[config(default = 4)]
     pub workers: usize,
-    #[config(default = 42)]
     pub seed: u64,
-    pub opt: AdamConfig,
-    #[config(default = 360)]
+    pub opt: AdamWConfig,
     pub batch_size: usize,
 }
-
+impl Default for CrossModels{
+    fn default() -> Self {
+        Self{
+            lstm: NucLstmConfig::default(),
+            bi_lstm: NucBiLstmConfig::default(),
+            stacked_lstm: StackedLstmConfig::default(),
+            stacked_bi_lstm: StackedBiLstmConfig::default(),
+            seq2seq: Seq2SeqConfig::default(),
+            num_epoch: 30,
+            workers: 4,
+            seed: 42,
+            opt:AdamWConfig::new().with_weight_decay(1e-3),
+            batch_size: 360,
+        }
+    }
+}
 impl CrossModels{
 
     #[allow(unused_must_use)]
@@ -111,23 +123,84 @@ impl CrossModels{
         self.create_artifact_dir::<B>(
             artifact_dir,
         );
-        let model = self.model.init::<B>(device.clone());
+        // define the models for training
+        let lstm_model = self.lstm.init::<B>(device.clone());
+        let bi_lstm_model = self.bi_lstm.init::<B>(device.clone());
+        let stackedlstm_model = self.stacked_lstm.init::<B>(device.clone());
+        let stackedbilstm_model = self.stacked_bi_lstm.init::<B>(device.clone());
+        let seq2seq_model = self.seq2seq.init::<B>(device.clone());
+
         let (train_loader, test_loader) = self.prepare_training::<B>(train_data, test_data, &device);
         // Initiate the training
-        let train = SupervisedTraining::new(artifact_dir, train_loader, test_loader)
-            .metric_train_numeric(LossMetric::new())
-            .metric_valid_numeric(LossMetric::new())
-            .with_file_checkpointer(CompactRecorder::new())
-            .num_epochs(self.num_epoch)
-            .summary();
-        let result = train.launch(Learner::new(model, self.opt.init(), 1e-3));
+        // train lstm
+        let result_lstm=||{
+             SupervisedTraining::new(artifact_dir, train_loader.clone(), test_loader.clone())
+                .metric_train_numeric(LossMetric::new())
+                .metric_valid_numeric(LossMetric::new())
+                .with_file_checkpointer(CompactRecorder::new())
+                .num_epochs(self.num_epoch)
+                .summary()
+                .launch(Learner::new(lstm_model, self.opt.init(), 1e-2))
+         };
+
+        //train_bilstm
+        let result_bilstm=||{
+             SupervisedTraining::new(artifact_dir, train_loader.clone(), test_loader.clone())
+                .metric_train_numeric(LossMetric::new())
+                .metric_valid_numeric(LossMetric::new())
+                .with_file_checkpointer(CompactRecorder::new())
+                .num_epochs(self.num_epoch)
+                .summary()
+                .launch(Learner::new(bi_lstm_model, self.opt.init(), 1e-2))
+         };
+        // stacked lstm
+        let result_stackedlstm=||{
+             SupervisedTraining::new(artifact_dir, train_loader.clone(), test_loader.clone())
+                .metric_train_numeric(LossMetric::new())
+                .metric_valid_numeric(LossMetric::new())
+                .with_file_checkpointer(CompactRecorder::new())
+                .num_epochs(self.num_epoch)
+                .summary()
+                .launch(Learner::new(stackedlstm_model, self.opt.init(), 1e-2))
+         };
+        // stacked bilstm
+        let result_stackedbilstm=||{
+             SupervisedTraining::new(artifact_dir, train_loader.clone(), test_loader.clone())
+                .metric_train_numeric(LossMetric::new())
+                .metric_valid_numeric(LossMetric::new())
+                .with_file_checkpointer(CompactRecorder::new())
+                .num_epochs(self.num_epoch)
+                .summary()
+                .launch(Learner::new(stackedbilstm_model, self.opt.init(), 1e-2))
+         };
+        // seq2seq model
+        let result_seq2seq=||{
+             SupervisedTraining::new(artifact_dir, train_loader.clone(), test_loader.clone())
+                .metric_train_numeric(LossMetric::new())
+                .metric_valid_numeric(LossMetric::new())
+                .with_file_checkpointer(CompactRecorder::new())
+                .num_epochs(self.num_epoch)
+                .summary()
+                .launch(Learner::new(seq2seq_model, self.opt.init(), 1e-2))
+         };
         //TODO: save the configurations
         self.save(format!("{artifact_dir}/config.json").as_str())
             .unwrap();
         //TODO: Save the model results
-        result
-            .model
-            .save_file(format!("{artifact_dir}/model"), &CompactRecorder::new())
+        result_lstm().model
+            .save_file(format!("{artifact_dir}/lstm_model"), &CompactRecorder::new())
+            .expect("Error in saving the trained model");
+        result_bilstm().model
+            .save_file(format!("{artifact_dir}/bilstm_model"), &CompactRecorder::new())
+            .expect("Error in saving the trained model");
+        result_stackedlstm().model
+            .save_file(format!("{artifact_dir}/stacked_lstm_model"), &CompactRecorder::new())
+            .expect("Error in saving the trained model");
+        result_stackedbilstm().model
+            .save_file(format!("{artifact_dir}/stacked_bi_lstm_model"), &CompactRecorder::new())
+            .expect("Error in saving the trained model");
+        result_seq2seq().model
+            .save_file(format!("{artifact_dir}/seq2seq_model"), &CompactRecorder::new())
             .expect("Error in saving the trained model");
     }
 
@@ -146,7 +219,7 @@ impl CrossModels{
         let batcher = NrelBatcher::new(device.clone());
         let test_batcher = NrelBatcher::<B::InnerBackend>::new(device.clone());
         // Train Data
-        let train_loader = DataLoadeer::new(batcher)
+        let train_loader = DataLoaderBuilder::new(batcher)
             .batch_size(self.batch_size)
             .num_workers(self.workers)
             .shuffle(self.seed)
