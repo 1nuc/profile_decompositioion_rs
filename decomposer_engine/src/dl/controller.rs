1f02c85573d9026db3ea7377427b3ce5a54e80c9
1nuc <ahmedalyafai85@gmail.com>
2026-05-08 17:03:07 +0800

    save notes

--- decomposer_engine/src/dl/controller.rs
+++ decomposer_engine/src/dl/controller.rs
@@ -1,13 +1,3 @@
-//TODO: Define the logic for training
-//TODO: Define the logic for testing and validation
-//TODO: Must make a function to split the dat into train, test and split
-//TODO: steps:
-//1. One struct that contains the data that will be red through the data engine
-//2. define the default implementation for the controller, the data backend, and many more
-//3. method for training and predicting
-//4. catch the method for the metrics
-//5. function to recieve the building input and process or forward the output
-
 use std::{
     fs::{File, copy, create_dir, read_dir, remove_dir_all}, path::{Path, PathBuf}, process::{Command}
 };
